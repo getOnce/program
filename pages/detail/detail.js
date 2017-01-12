bca76a00439ca81bf0a7ad53b1c10980ec6ec64f
liujianchuan <jianchuan.liu@qyer.com>
2017-01-12 17:13:20 +0800

    小程序

---
+++ pages/detail/detail.js
@@ -0,0 +1,8 @@
+Page({
+    data: {
+        title: '标题测试数据',
+        price: '12元起',
+        content: '中华人民共和国中华人民共和国中华人民共和国',
+        src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/0.jpg'
+    }
+})
---
+++ pages/test/test.js
@@ -0,0 +1,68 @@
+var hotels = [
+    {
+      place: false,
+      title: 'USA宾馆',
+      price: '3661元起',
+      src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/0.jpg',
+      id: 1
+    },
+    {
+      place: 'CHN',
+      title: 'CHN宾馆',
+      price: '366元起',
+      src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/0.jpg',
+      id: 2
+    },
+    {
+      place: 'BRA',
+      title: 'BRA宾馆',
+      price: '366元起',
+      src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/0.jpg',
+      id: 3
+    },
+    {
+      place: 'USA',
+      title: 'JPN宾馆',
+      price: '366元起',
+      src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/0.jpg',
+      id: 4
+    },
+    {
+      place: 'ENG',
+      title: 'USA宾馆',
+      price: '3662元起',
+      src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/0.jpg',
+      id: 5
+    },
+    {
+      place: 'ENG',
+      title: 'USA宾馆',
+      price: '3663元起',
+      src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/0.jpg',
+      id: 6
+    }
+  ]
+Page({
+  data: {
+    items: [
+      {name: 'USA', value: '美国'},
+      {name: 'CHN', value: '中国', checked: 'true'},
+      {name: 'BRA', value: '巴西'},
+      {name: 'JPN', value: '日本'},
+      {name: 'ENG', value: '英国'},
+      {name: 'TUR', value: '法国'},
+    ],
+    hotelArray: [{}, {}, {}, {}, {}, {}]
+  },
+  checkboxChange: function(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  },
+  onLoad: function(){
+    var me = this;
+    setTimeout(function(){
+      me.setData({
+        hotelArray: hotels
+      })
+    }, 2000)
+  }
+})
